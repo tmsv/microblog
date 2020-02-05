83eb0ef8f51e48b436941b23298f30babc02c652
tmsv <42784577+tmsv@users.noreply.github.com>
2020-02-05 16:09:13 +0300

    refactored unit testing

--- app/email.py
+++ app/email.py
@@ -5,13 +5,13 @@ from app import mail
 
 
 def send_async_email(app, msg):
-	with app.app_context():
-		mail.send(msg)
+    with app.app_context():
+        mail.send(msg)
 
 
 def send_email(subject, sender, recipients, text_body, html_body):
-	msg = Message(subject, sender=sender, recipients=recipients)
-	msg.body = text_body
-	msg.html = html_body
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
     Thread(target=send_async_email,
-       args=(current_app._get_current_object(), msg)).start()
+           args=(current_app._get_current_object(), msg)).start()
--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from time import time
+from flask import current_app
 import jwt
-from app import app, db, login
+from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
@@ -66,7 +67,7 @@ class User(UserMixin, db.Model):
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
         except:
             return
